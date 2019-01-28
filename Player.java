31bebade8331fe8851f48f9619c68e7f9e3b4d72
Miko <mikoto.uwu@gmail.com>
2019-01-29 01:25:01 +0530

    PLAYER SPRITE MOVES
    finally! the game loop is fixed and although there was no flashing screen observed earlier it may be because of my CPU performance. Player sprite is now capable of moving

--- Player.java
+++ Player.java
@@ -17,17 +17,19 @@ public class Player extends GameObject {
     public Player(int x, int y, ID id) {
         super(x, y, id);
         //setX(100);
+        velX = 1;
     }
 
     @Override
     public void tick() {
-
+        x += velX;
+        y += velY;
     }
 
     @Override
     public void render(Graphics g) {
         g.setColor(Color.ORANGE);
-        g.fillRect(x, y, 32, 32); 
+        g.fillRect(x, y, 32, 32);
     }
-    
+
 }
