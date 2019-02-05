f72b69ce7729ec9c26320d8070dc815881524ef3
Miko <mikoto.uwu@gmail.com>
2019-02-05 21:41:58 +0530

    Added green to red transition to HUD

--- HUD.java
+++ HUD.java
@@ -6,10 +6,15 @@ import java.awt.Graphics;
 public class HUD {
 
     public static int HEALTH = 100;
+    private int greenValue = 255;
 
     public void tick() {
-        
+
         HEALTH = Game.clamp(HEALTH, 0, HEALTH); //constraints
+
+        greenValue = HEALTH * 2; //adjust to health bar
+        greenValue = Game.clamp(greenValue, 0, 255);
+
     }
 
     public void render(Graphics g) {
@@ -17,7 +22,7 @@ public class HUD {
         g.fill3DRect(20, 20, 200, 20, true);
         //@20,20 w/ 200 width 20 height, RAISED = embossed = true
 
-        g.setColor(Color.red);
+        g.setColor(new Color(120, greenValue, 0)); //RGB 
         g.fill3DRect(20, 20, HEALTH * 2, 20, true);
         //this fills another rectangle with actual health status
 
